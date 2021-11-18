ac239338a3bff950652462804982178663036f4f
taylor-stack <94589446+taylor-stack@users.noreply.github.com>
2021-11-17 23:59:12 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hi! Ready to start a band?")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name is " + city + " " + pet)
